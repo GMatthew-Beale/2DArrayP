ba549e94d3bb25976ceebfc12c3c9cb23728e28a
george.beale <george.beale@mu-c1-17.mullen.pvt.k12.co.us>
2018-10-11 14:57:33 -0600

    Add Database and Add Board

--- 2DArrayP/src/Runner.java
+++ 2DArrayP/src/Runner.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 
 public class Runner
 	{
 
 		public static void main(String[] args)
 			{
-			
+				Scanner userInput = new  Scanner(System.in);
+				System.out.println("Enter Name to Play");
+				String name = userInput.nextLine();
+				System.out.println("Hello," + name + "!" + " Match the cards");
+System.out.println("             A1      A2       A3      A4     A5      A6      A7");
+System.out.println("          :-------:-------:-------:-------:-------:-------:-------:");
+System.out.println("          :       :       :       :       :       :       :       :");
+System.out.println("          :       :       :       :       :       :       :       :");
+System.out.println("          :		  :		  :   	  :       :		  :		  :		  :");				
+System.out.println("          :-------:-------:-------:-------:-------:-------:-------:");
+System.out.println("          :       :       :       :       :       :       :       :");
+System.out.println("          :       :       :       :       :       :       :       :");
+System.out.println("          :       :       :       :       :       :       :       :");
+System.out.println("          :-------:-------:-------:-------:-------:-------:-------:");
+System.out.println("             B1      B2      B3      B4       B5      B6      B7");
+
+
+
+
+
 
 			}
 
