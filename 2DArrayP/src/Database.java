ffd0c863b817e258240d23f15c77e73533a3b8ed
george.beale <george.beale@mu-c1-17.mullen.pvt.k12.co.us>
2018-10-17 10:20:49 -0600

    Add Nested ForLoop

--- 2DArrayP/src/Database.java
+++ 2DArrayP/src/Database.java
@@ -1,43 +1,88 @@
 import java.util.ArrayList;
 import java.util.Collections;
+
 public class Database
 	{
-		
-	static String screen[ ] [ ] = new String [2] [7]; 	
-	static String answers[ ] [ ] = new String [2] [7]; 	
 
-	static ArrayList<String> words = new ArrayList<String>();
-	public static void addWords()
-		{
-		words.add("Dog");
-		words.add("Cat");
-		words.add("Cow");
-		words.add("Pig");
-		words.add("Rat");
-		words.add("Bat");
-		words.add("Dog");
-		words.add("Cat");
-		words.add("Bee");
-		words.add("Cow");
-		words.add("Pig");
-		words.add("Rat");
-		words.add("Bat");
-		words.add("Bee");
-
-Collections.shuffle(words);
-int index = 0;
-for (int row = 0; row < 2; row++)
-	{
-		for (int col = 0; col < 7; col++)
+		public static void main(String[] args)
 			{
-				answers [row] [col] = words.get(index);
+			//	test();
+				addWords();
 			}
-		index++;
+
+		
+
+		static String screen[][] = new String[2][7];
+		static String answers[][] = new String[2][7];
+
+		static ArrayList<String> words = new ArrayList<String>();
+
+		public static void addWords()
+			{
+				words.add("Dog");
+				words.add("Cat");
+				words.add("Cow");
+				words.add("Pig");
+				words.add("Rat");
+				words.add("Bat");
+				words.add("Dog");
+				words.add("Cat");
+				words.add("Bee");
+				words.add("Cow");
+				words.add("Pig");
+				words.add("Rat");
+				words.add("Bat");
+				words.add("Bee");
+
+				Collections.shuffle(words);
+				int index = 0;
+				for (int row = 0; row < 2; row++)
+					{
+						for (int col = 0; col < 7; col++)
+							{
+								answers[row][col] = words.get(index);
+
+								index++;
+							}
+					}
+			}
+				
+				
+				
+				
+				public static void test()
+					{
+				for (int row = 0; row < 2; row++)
+					{
+						for (int col = 0; col < 7; col++)
+							{
+								System.out.print(answers[row][col] + "   ");
+							}
+					//	index++;
+						System.out.println();
+					}
+
+			}
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-
-
-
-		}
-	}
